ffed784648ddbfb4426566263d07b7cbc57b66d3
Programer308 <160789430+Programer308@users.noreply.github.com>
2024-11-21 15:37:14 +0300

    Create DocumentNode.java

---
+++ DocumentNode.java
@@ -0,0 +1,12 @@
+public class DocumentNode <T> {
+    String docId;
+    DocumentNode <T> next;
+    LinkedList<String> words;
+
+    DocumentNode(String docId, int score) {
+        this.docId = docId;
+        this.words = words;
+        this.next = null;
+    }
+} // [id,score]
+ // 
