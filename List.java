4af18280fbb22f4679cd2dcee9e7d5d31dffea04
Mohammed <v7mv123@gmail.com>
2024-11-23 23:03:16 +0300

    added the last missing classes

---
+++ List.java
@@ -0,0 +1,10 @@
+
+public interface List<T> {
+    public void findFirst();
+    public void findNext();
+    public String retrieve();
+    public void insert(String e);
+    public boolean empty();
+    public boolean last();
+    }
+    
---
+++ Stack.java
@@ -0,0 +1,8 @@
+
+public interface Stack<T> {
+	
+	public void push(T e);
+	public T pop();
+	public boolean full();
+	public boolean empty();
+}
